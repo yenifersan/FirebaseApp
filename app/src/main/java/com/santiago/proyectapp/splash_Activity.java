5b462d5a32c14ecc864e5f37c90dfc004bd3576a
yenifer <yenifer.santiago@tecsup.edu.pe>
2019-04-25 09:54:34 -0500

    Commit Sprint 01 Yenifer

---
+++ app/src/main/java/com/santiago/proyectapp/splash_Activity.java
@@ -0,0 +1,27 @@
+package com.santiago.proyectapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esplash_);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=  new Intent(
+                        splash_Activity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
---
+++ app/src/main/java/com/santiago/proyectapp/unoActivity.java
@@ -0,0 +1,13 @@
+package com.santiago.proyectapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class unoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uno);
+    }
+}
